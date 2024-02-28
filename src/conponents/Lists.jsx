163244eab0f8ee64b457abc3e00fc4298b6b4f7b
Craig Moore <craig-moore22@outlook.com>
2024-02-28 11:42:29 +0000

    movie list update with sort method

--- src/conponents/Lists.jsx
+++ src/conponents/Lists.jsx
@@ -1,12 +1,16 @@
 function List() {
-  const movies = ["saving private ryan", "Hush", "Inception", "The Batman"];
-  const myMovieList = movies.map((myMovieList) => (
-    <>
-      <li>{myMovieList}</li>
-    </>
-  ));
-  
-  return (<ul>{myMovieList}</ul>);
+  const movies = [
+    {id: 1, name: "The Batman", rating: 9},
+    {id: 2, name: "Saving Private Ryan", rating: 10},
+    {id: 3, name: "BladeRunner", rating: 8 },
+    {id: 4,name: "Drag Me to Hell", rating: 2},
+  ];
+
+  movies.sort((a, b) => b.rating - a.rating);
+
+  const listItems = movies.map((movie) => <li key={movie.id}>{movie.name}: {movie.rating}</li>);
+
+  return (<ul>{listItems}</ul>);
 }
 
 export default List;
